49aac34390da362ed4689d0fc43c5c7b3b6145a4
Klevakin Sergey <justerest@yandex.ru>
2017-03-06 18:38:19 +0500

    need to check!!!

--- app.js
+++ app.js
@@ -2,16 +2,16 @@ const jsonApi = require("jsonapi-server"),
     app = jsonApi.getExpressServer(),
     appSettings = require('./lib/app-settings.js'),
     bearerSettings = require('./lib/bearer-settings.js'),
+    resources = require('./lib/resources.js'),
     routing = require('./lib/routing.js');
-
-//define models
-require('./lib/resources.js');
 
 jsonApi.setConfig({
     port: 443,
     graphiql: false,
     base: 'api'
 });
+
+resources(jsonApi);
 
 appSettings(app);
 
